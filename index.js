81b98bc4eb6f88243745c1990bdb688295d0afba
Walker Le <walker.le88@gmail.com>
2022-12-29 10:28:53 -0700

    Initial commit, setup HTML, JS, and CSS files

---
+++ index.js
@@ -0,0 +1,14 @@
+// Globals
+const baseUrl = "https://goweather.herokuapp.com/weather/"
+
+// DOM Selectors
+
+// Fetch Functions
+
+// Back-end Fetch Functions
+
+// Event Listeners
+
+// Event Handlers
+
+// Initializers
