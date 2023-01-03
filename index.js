49dffc2ae51f21c627d697184d93ec7c8e80fdb0
Walker Le <walker.le88@gmail.com>
2023-01-02 21:31:45 -0700

    Updated JS with function placeholders

--- index.js
+++ index.js
@@ -2,13 +2,38 @@
 const baseUrl = "https://goweather.herokuapp.com/weather/"
 
 // DOM Selectors
+const openCityForm = document.querySelector('');
+const cityForm = document.querySelector('');
+const commentsForm = document.querySelector('');
+const catHTML = document.querySelector('');
 
 // Fetch Functions
+function getApiData(url) {
+    return fetch(url)
+    .then(res => res.json())
+}
+
+function getOneCityData(url, city) {
+    return fetch(`${url}/${city}`)
+    .then(res => res.json())
+}
 
 // Back-end Fetch Functions
 
+// Render Functions
+function renderCityData(data) {}
+
+function renderForecast(data) {}
+
 // Event Listeners
+openCityForm.addEventListener('click', cycleCatPics);
+cityForm.addEventListener('submit', cycleCatPics);
+commentsForm.addEventListener('submit', cycleCatPics);
+catHTML.addEventListener('dblclick', cycleCatPics);
 
 // Event Handlers
+function cycleCatPics() {}
 
 // Initializers
+getApiData(baseUrl).then(data => console.log(data)); // renderForecast?
+getOneCityData(baseUrl, 'New York').then(data => console.log(data)); // renderCityData?
